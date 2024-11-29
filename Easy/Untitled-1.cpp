97b5152dfd71bdda5fa15f00abe9a0a17c71fa0f
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-11-29 13:44:06 +0600

    Add files via upload

---
+++ Easy/Untitled-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& num) {
+     int ans=0;
+     for(int i:num)
+     {
+         ans=ans^i;
+
+     }
+     return ans;
+        
+    }
+};
+}
+
